8d4dca00247c2f9613ff28f056138cf013f215af
Jihad Hossain <78225332+jihad-hossain1@users.noreply.github.com>
2024-05-01 22:57:59 +0600

    login with signup page ui done

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+'use client';
+
+import Image from 'next/image';
+import React, { useState } from 'react'
 
 const SignUpPage = () => {
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState('')
+    const [formData, setFormData] = useState({
+        fullname: "",
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+    }
     return (
-        <div>SignUpPage</div>
+        <main className='max-w-screen-xl m-auto flex justify-center items-center min-h-[80vh] my-4'>
+            <div className='bg-gradient-to-b from-[#C2DAFF] to-[#EFF1F0] rounded-2xl shadow-md drop-shadow-sm border border-gray-50 p-5 md:p-20 lg:p-[80px]'>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+                    <div className='flex justify-center mb-7'>
+                        <Image src={"/logo.png"} alt="logo" width={200} height={200} />
+                    </div>
+                    <input required={true} type="text" value={formData.fullname} onChange={handleChange} name="fullname" placeholder="Full Name" className="inpt" id="fullname" />
+
+                    <input required={true} type="email" value={formData.email} onChange={handleChange} name="email" placeholder="Email Address" className="inpt" id="email" />
+
+                    <input required={true} value={formData.password} type="password" onChange={handleChange} name="password" placeholder="Password" className="inpt" id="password" />
+
+                    <button type="submit" className="btn" disabled={loading}>
+                        {loading ? "Loading..." : "Signup"}
+                    </button>
+                </form>
+            </div>
+        </main>
     )
 }
 
--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,38 +1,59 @@
 'use client'
 
 import React from "react";
-import Image from "next/image";
 import AnimOne from "@/components/ui/AnimOne";
 import { IoMdMail } from "react-icons/io";
 import { FaUserLarge } from "react-icons/fa6";
 
 
+
 const ContactUsPage = () => {
+    const [loading, setLoading] = React.useState(false);
+    const [errors, setErrors] = React.useState('');
+    const [formData, setFormData] = React.useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    }
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
         <main>
-            <div className="bg-[#3366CC] py-4 text-white px-6 flex justify-between items-center">
+            <div className=" bg-[#3366CC] py-4 text-white px-6 flex flex-col gap-2 md:justify-between md:items-center md:flex-row">
                 <h1 className="text-3xl font-bold uppercase">Demo Logo</h1>
                 <div>
-                    <input type="text" name="" id="" placeholder="Search" className="bg-white text-gray-600 px-4 py-2 rounded-md focus:outline-none max-w-[600px] min-w-[400px]" />
+                    <input type="text" name="" id="" placeholder="Search" className="bg-white text-gray-600 px-4 py-2 rounded-md focus:outline-none md:max-w-[600px] md:min-w-[400px]" />
                 </div>
             </div>
 
             <section className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8 items-center my-10">
-                <div>
+                <form onSubmit={handleSubmit}>
                     <h4 className="text-3xl md:text-5xl font-bold text-gray-600 ">Contact Us</h4>
                     <div className="mt-8 flex flex-col gap-4">
                         <div className="relative">
-                            <input type="text" name="name" placeholder="Name" className="bg-gray-100 border border-gray-300 pl-10 py-2 rounded-2xl focus:outline-none w-full" id="name" />
+                            <input type="text" name="name" placeholder="Name" className="bg-gray-100 border border-gray-300 pl-10 py-2 rounded-2xl focus:outline-none w-full" id="name" onChange={handleChange} />
                             <FaUserLarge className="absolute top-1/2 left-4 -translate-y-1/2 text-gray-400" />
                         </div>
                         <div className="relative">
-                            <input type="email" name="email" placeholder="Email" className="bg-gray-100 border border-gray-300 pl-10 py-2 rounded-2xl focus:outline-none w-full" id="email" />
+                            <input type="email" name="email" placeholder="Email" className="bg-gray-100 border border-gray-300 pl-10 py-2 rounded-2xl focus:outline-none w-full" id="email" onChange={handleChange} />
                             <IoMdMail className="absolute top-1/2 left-4 -translate-y-1/2 text-gray-400" />
                         </div>
-                        <textarea name="message" id="message" cols={20} rows={10} placeholder="Message" className="bg-gray-100 border border-gray-300 px-4 py-2 rounded-2xl focus:outline-none" />
-                        <button className="bg-[#3366CC] text-white px-4 py-3 rounded-xl focus:outline-none">Send Message</button>
+                        <textarea onChange={handleChange} name="message" id="message" cols={20} rows={10} placeholder="Message" className="bg-gray-100 border border-gray-300 px-4 py-2 rounded-2xl focus:outline-none" />
+                        <button type="submit" className="bg-[#3366CC] text-white px-4 py-3 rounded-xl focus:outline-none">Send Message</button>
                     </div>
-                </div>
+                </form>
                 <div>
                     <AnimOne />
                 </div>
